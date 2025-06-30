5ca7dcb6e94fbf73027dbe34d49b81203473b114
Azeddine Bedar <azzedinebedar@gamil.com>
2025-06-30 14:27:55 +0100

    I added transform between Home & projects

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,35 @@ import NavBottom from "./components/NavBottom";
 // GSAP
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
+// GSAP Plugin
+gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(useGSAP);
 
 function App() {
   const navRef = useRef(null);
   const heroRef = useRef(null);
   const b_navRef = useRef(null);
+  const homeRef = useRef(null);
+  const projectsRef = useRef(null);
+  const containerRef = useRef(null);
 
   // Animation GSAP
   useGSAP(() => {
+    // GSAP Timeline
     const tl = gsap.timeline();
     const heroTl = gsap.timeline();
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        pin: true,
+        pinSpacing: false,
+      },
+    });
 
     heroTl.from(".text2", {
       opacity: 0,
@@ -44,15 +61,49 @@ function App() {
     tl.from(navRef.current, { opacity: 0, y: -50, duration: 1 });
     tl.add(heroTl, "+=0.2");
     tl.from(b_navRef.current, { opacity: 0, y: 50, duration: 1 }, "-=0.8");
+
+    // Scroll Gsap
+    scrollTl.to(homeRef.current, {
+      opacity: 0,
+      scale: 4,
+      duration: 2,
+      ease: "power2.out",
+    });
+
+    scrollTl.fromTo(
+      projectsRef.current,
+      { opacity: 0, scale: 0 },
+      {
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+        ease: "power2.out",
+      }
+    );
   });
   return (
-    <div className="min-h-screen flex flex-col">
-      <NavTop ref={navRef} />
-      <main className="flex-grow flex items-center justify-center">
-        <Home ref={heroRef} />
-        {/* <Projects /> */}
-      </main>
-      <NavBottom ref={b_navRef} />
+    <div className="min-h-screen">
+      <div className="fixed top-0 left-0 w-full z-50">
+        <NavTop ref={navRef} />
+      </div>
+      <section className="h-screen relative overflow-hidden" ref={containerRef}>
+        <div
+          ref={homeRef}
+          className="absolute inset-0 scale-100 opacity-100 flex items-center justify-center"
+        >
+          <Home ref={heroRef} />
+        </div>
+        <div
+          ref={projectsRef}
+          className="absolute inset-0 opacity-0 scale-0 flex items-center justify-center"
+        >
+          <Projects />
+        </div>
+      </section>
+      <div className="h-screen"></div>
+      <div className="fixed bottom-0 left-0 w-full z-50">
+        <NavBottom ref={b_navRef} />
+      </div>
     </div>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ const dataSlider = [
     title: "Title",
     description: "Description",
     image:
-      "https://s3-alpha.figma.com/hub/file/5999957204/dd434a82-8a1b-45b3-a5e0-84057a10ba17-cover.png",
+      "https://plus.unsplash.com/premium_photo-1750355751988-ac2f0cbc335c?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 2,
@@ -47,52 +47,54 @@ const Projects = () => {
   const getPositionClass = (index) => {
     if (index === activeIndex) return "z-20 scale-110";
     if (index === (activeIndex - 1 + dataSlider.length) % dataSlider.length)
-      return "z-10 -translate-x-70 opacity-60 scale-90";
+      return "z-10 md:-translate-x-70 -translate-x-10 opacity-60 scale-90";
     if (index === (activeIndex + 1) % dataSlider.length)
-      return "z-10 translate-x-70 opacity-60 scale-90";
+      return "z-10 md:translate-x-70 translate-x-10 opacity-60 scale-90";
     return "hidden";
   };
 
   return (
-    <section className="flex-grow relative flex justify-center items-center bg-black">
-      <button
-        onClick={handlePrev}
-        className="absolute left-4 text-white text-4xl z-30"
-      >
-        ‹
-      </button>
+    <>
+      <section className="flex flex-grow relative justify-center items-center bg-black">
+        <button
+          onClick={handlePrev}
+          className="absolute left-4 text-white text-4xl z-30"
+        >
+          ‹
+        </button>
 
-      <div className="relative flex justify-center items-center w-full max-w-5xl h-[500px]">
-        {dataSlider.map((data, index) => (
-          <div
-            key={data.id}
-            className={`absolute transition-all duration-500 ease-in-out transform ${getPositionClass(
-              index
-            )}`}
-          >
-            <img
-              src={data.image}
-              alt={data.title}
-              className="md:w-[40vw] md:h-[44vh] object-cover rounded-lg shadow-2xl"
-            />
+        <div className="relative flex justify-center items-center w-full max-w-5xl h-[500px]">
+          {dataSlider.map((data, index) => (
+            <div
+              key={data.id}
+              className={`absolute transition-all duration-500 ease-in-out transform ${getPositionClass(
+                index
+              )}`}
+            >
+              <img
+                src={data.image}
+                alt={data.title}
+                className="md:w-[40vw] md:h-[44vh] w-[82vw] h-[32vh] object-cover rounded-lg shadow-2xl hover:rotate-2 transform duration-300"
+              />
 
-            {index === activeIndex && (
-              <div className="text-center mt-4 text-white">
-                <h2 className="text-2xl font-bold">{data.title}</h2>
-                <p className="text-gray-300">{data.description}</p>
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
+              {index === activeIndex && (
+                <div className="text-center mt-4 text-white">
+                  <h2 className="text-2xl font-bold">{data.title}</h2>
+                  <p className="text-gray-300">{data.description}</p>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
 
-      <button
-        onClick={handleNext}
-        className="absolute right-4 text-white text-4xl z-30"
-      >
-        ›
-      </button>
-    </section>
+        <button
+          onClick={handleNext}
+          className="absolute right-4 text-white text-4xl z-30"
+        >
+          ›
+        </button>
+      </section>
+    </>
   );
 };
 
